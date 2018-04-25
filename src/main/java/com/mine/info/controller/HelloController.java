0e49398b89038ba5a20ccae16ae6ec169b2655f4
mayee <yelekeri@gmail.com>
2018-04-24 21:26:39 -0400

    hellocontoller checkstyle fixed

--- src/main/java/com/mine/info/controller/HelloController.java
+++ src/main/java/com/mine/info/controller/HelloController.java
@@ -1,8 +1,5 @@
 package com.mine.info.controller;
 
-import javax.ws.rs.Path;
-
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,11 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
+	public static final String name = "name";
+	public static final String defaultValue = "World"; 
+	public static final String url = "/hello"; 
+	final String returnValue = "hello"; 
+	final String attributeName = "user"; 
+	
+    @GetMapping(url)
     public String sayHello(
        @RequestParam(value = "name", required = false,
-                          defaultValue = "World") String name, Model model) {
-        model.addAttribute("user", name);
-        return "hello";
+                          defaultValue = defaultValue) String name, Model model) {
+        model.addAttribute(attributeName, name);
+        return returnValue;
     }
 }
