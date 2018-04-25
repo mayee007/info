30c0bc651ec842cff0c23acd788731afdced3d1a
mayee <yelekeri@gmail.com>
2018-04-24 23:47:41 -0400

    added package-info.java

--- src/main/java/com/mine/info/controller/HelloController.java
+++ src/main/java/com/mine/info/controller/HelloController.java
@@ -11,16 +11,16 @@ public class HelloController {
 	// public static final String name = "name";
 	// Can't pass this as a variable, getting this error
 	// java element value must be a constant expression
-	
-	public static final String defaultValue = "World";
-	public static final String url = "/hello";
-	final String returnValue = "hello";
-	final String attributeName = "user";
-	
-    @GetMapping(url)
+
+	public static final String DEFAULT_VALUE = "World";
+	public static final String URL = "/hello";
+	private final String returnValue = "hello";
+	private final String attributeName = "user";
+
+    @GetMapping(URL)
     public String sayHello(
        @RequestParam(value = "name", required = false,
-                     defaultValue = defaultValue) String name, Model model) {
+                     defaultValue = DEFAULT_VALUE) String name, Model model) {
         model.addAttribute(attributeName, name);
         return returnValue;
     }
