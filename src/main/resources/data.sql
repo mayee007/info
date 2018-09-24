a188a4c47c6831f0b76d197520fe4d7b4d92fc52
mayee <yelekeri@gmail.com>
2018-09-24 07:55:07 -0400

    h2 db added

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into technology (category, technologytype) values('java', 'spring');
+insert into technology (category, technologytype) values('java', 'dwr');
+insert into technology (category, technologytype) values('java','hibernate');
+
+insert into info (description, modifieddate, subject, submitdate, technology) 
+values ('test' , current_timestamp, 'test', current_timestamp, 1); 
+insert into info (description, modifieddate, subject, submitdate, technology) 
+values ('test' , current_timestamp, 'test', current_timestamp, 2);
+
+insert into problem (modifieddate, problem, reasonforproblem, solution, submitdate, technology) 
+values (current_timestamp, 'test' , 'test' , 'test', current_timestamp, 2);
