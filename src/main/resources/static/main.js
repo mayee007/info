91a3d7b5b0dc94d7e6c10d73da16b519e72f3717
mayee007 <yelekeri@gmail.com>
2018-10-04 21:45:59 -0400

    angular files

---
+++ src/main/resources/static/main.js
@@ -0,0 +1,98 @@
+var app = angular.module("TechnologyManagement", []);
+ 
+// Controller Part
+app.controller("TechnologyController", function($scope, $http) {
+ 
+ 
+    $scope.Technologys = [];
+    $scope.TechnologyForm = {
+        technologyId: 1,
+        technologyType: "",
+        category: ""
+    };
+ 
+    // Now load the data from server
+    _refreshTechnologyData();
+ 
+    // HTTP POST/PUT methods for add/edit Technology  
+    // Call: http://localhost:8080/Technology
+    $scope.submitTechnology = function() {
+ 
+        var method = "";
+        var url = "";
+ 
+        if ($scope.TechnologyForm.technologyId == -1) {
+            method = "POST";
+            url = '/technology';
+        } else {
+            method = "PUT";
+            url = '/technology';
+        }
+ 
+        $http({
+            method: method,
+            url: url,
+            data: angular.toJson($scope.TechnologyForm),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(_success, _error);
+    };
+ 
+    $scope.createTechnology = function() {
+        _clearFormData();
+    }
+ 
+    // HTTP DELETE- delete Technology by Id
+    // Call: http://localhost:8080/Technology/{empId}
+    $scope.deleteTechnology = function(Technology) {
+        $http({
+            method: 'DELETE',
+            url: '/technology/' + Technology.technologyId
+        }).then(_success, _error);
+    };
+ 
+    // In case of edit
+    $scope.editTechnology = function(Technology) {
+        $scope.TechnologyForm.technologyId = Technology.technologyId;
+        $scope.TechnologyForm.technologyType = Technology.technologyType;
+        $scope.TechnologyForm.category = Technology.category;
+    };
+ 
+    // Private Method  
+    // HTTP GET- get all Technologys collection
+    // Call: http://localhost:8080/Technologys
+    function _refreshTechnologyData() {
+        $http({
+            method: 'GET',
+            url: '/technology'
+        }).then(
+            function(res) { // success
+                $scope.Technologys = res.data;
+            },
+            function(res) { // error
+                console.log("Error: " + res.status + " : " + res.data);
+            }
+        );
+    }
+ 
+    function _success(res) {
+        _refreshTechnologyData();
+        _clearFormData();
+    }
+ 
+    function _error(res) {
+        var data = res.data;
+        var status = res.status;
+        var header = res.header;
+        var config = res.config;
+        alert("Error: " + status + ":" + data);
+    }
+ 
+    // Clear the form
+    function _clearFormData() {
+        $scope.TechnologyForm.technologyId = -1;
+        $scope.TechnologyForm.technologyType = "";
+        $scope.TechnologyForm.category = ""
+    };
+});
