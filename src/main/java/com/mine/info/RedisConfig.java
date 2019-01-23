79bf4a66da907270f7a86684c5439f4d4cd2749a
mayee007 <yelekeri@gmail.com>
2019-01-23 13:51:42 -0500

    redis factory passing redis config using app.properties

--- src/main/java/com/mine/info/RedisConfig.java
+++ src/main/java/com/mine/info/RedisConfig.java
@@ -1,8 +1,12 @@
 package com.mine.info;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration.JedisClientConfigurationBuilder;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
@@ -19,9 +23,24 @@ import com.mine.info.model.*;
 @ComponentScan("com.model")
 public class RedisConfig {
 
+	@Value("${spring.redis.host}")
+	private String redisHost;
+	@Value("${spring.redis.port}")
+	private int redisPort;
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
+        
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(redisHost);
+        redisStandaloneConfiguration.setPort(redisPort);
+        redisStandaloneConfiguration.setDatabase(0);
+        
+        JedisClientConfigurationBuilder jedisClientConfiguration = JedisClientConfiguration.builder();
+        
+        JedisConnectionFactory jedisConFactory = new JedisConnectionFactory(redisStandaloneConfiguration,
+                jedisClientConfiguration.build());
+
+        return jedisConFactory;
     }
 
     /* @Bean
