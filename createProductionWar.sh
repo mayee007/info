41737e8975d6dcbef0be0c93fbdb9f7e75c60e1e
Mahesh Mahadeva <yelekeri@gmail.com>
2019-03-15 05:06:32 +0000

    path

--- createProductionWar.sh
+++ createProductionWar.sh
@@ -8,4 +8,5 @@ cd extract
 jar xvf ../*.war
 rm -rf ../*.war 
 cp ~/.ssh/application.properties WEB-INF/classes/application.properties
+cp ~/.ssh/application.properties WEB-INF/classes/application-prod.properties
 jar cvf ../../$1 . 
